e30558a9ffbf74346178c758ab0cf5ecfcae019a
RyanMcNeilly85 <ryanmcn@live.com.au>
2022-05-28 14:20:40 +1000

    Initial commit

---
+++ src/Lambda.java
@@ -0,0 +1,16 @@
+/**
+ * <i>joe</i>
+ */
+public final class Lambda {
+
+    /**
+     * Private constructor for the class
+     */
+    private Lambda() {}
+
+    public static final double E = 2.7182818284590452354;
+
+    public static final double PI = 3.14159265358979323846;
+
+
+}
